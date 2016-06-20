22fc4784aed2a2bb1c9e483f20c992e2b43252d4
jackyhobingo <jackyhobingo@gmail.com>
2016-06-20 12:32:28 +0800

    簡單的獲得票數 性別 職位限制 和票數排序

--- js/compute.js
+++ js/compute.js
@@ -1,8 +1,32 @@
 var count = 4;
 var condition_count = 4;
 var candidate_count = 1;
-function compute() {
 
+function sortFunction(a,b){
+    return b.votes - a.votes;
+}
+function get_gender_limits(){
+    alert($('select#gender_limit').val());
+    alert(parseInt($('input#gender_limit_son').val()));
+    alert(parseInt($('input#gender_limit_mom').val()));
+}
+
+function get_position_limit(){
+    alert($('select#position_limit').val());
+    alert(parseInt($('input#position_limit_son').val()));
+    alert(parseInt($('input#position_limit_mom').val()));
+}
+
+function compute() {
+    var p = Array(candidate_count);
+    for (var i = 0; i < candidate_count; i++) {
+        p[i] = People(i+1);
+    }
+    p.sort(sortFunction)
+    for (var i = 0; i < candidate_count; i++) {
+        alert(p[i].name);
+        alert(p[i].votes);
+    }
 }
 
 function add_condition() {
@@ -14,28 +38,42 @@ function add_condition() {
 }
 
 function add_candidate(num) {
-	// $("tr.num"+num).after("<tr><td>1</td><tr>")
+	$("tr.num"+num).after('\
+        <tr class="num'+(num+1)+'">\
+            <td><input class="num'+(num+1)+'" id="num'+(num+1)+'_col1" type="text"></td>\
+            <td><input class="num'+(num+1)+' gender'+(num+1)+'" id="num'+(num+1)+'_col2" type=radio name="gender'+(num+1)+'" value="man"></td>\
+            <td class="gender'+(num+1)+'">男</td>\
+            <td><input class="num'+(num+1)+' gender'+(num+1)+'" id="num'+(num+1)+'_col2" type=radio value="woman" name="gender'+(num+1)+'"></td>\
+            <td class="gender'+(num+1)+'">女</td>\
+            <td><input class="num'+(num+1)+' position'+(num+1)+'" id="num'+(num+1)+'_col3" type=radio name="position'+(num+1)+'" value="normal"></td>\
+            <td class="position'+(num+1)+'">一般</td>\
+            <td><input class="num'+(num+1)+' position'+(num+1)+'" id="num'+(num+1)+'_col3" type=radio value="admin" name="position'+(num+1)+'"></td>\
+            <td class="position'+(num+1)+'">行政</td>\
+            <td><input class="num'+(num+1)+'" id="num'+(num+1)+'_col4" type="text"></td>\
+        </tr>')
+    candidate_count +=1;
 }
 
 function add_election_condition() {
 
 }
-
+function get_how_many_people_needed(){
+    return $('input#how_many').val();
+}
 function get_votes(i) {
-    alert(get_input_data(i, count, false, 0));
+    return parseInt(get_input_data(i, 4, false, 0));
 }
 
 function get_gender(i) {
-    alert(get_input_data(i, 2, true, "gender"));
+    return get_input_data(i, 2, true, "gender"+i);
 }
 
 function get_name(i) {
-	alert(100)
-    alert(get_input_data(i, 1, false, 0));
+    return get_input_data(i, 1, false, 0);
 }
 
 function get_position(i) {
-    alert(get_input_data(i, 3, true, "position"));
+    return get_input_data(i, 3, true, "position"+i);
 }
 
 function get_input_data(num, col, is_seletion, seletion_name) {
@@ -46,9 +84,23 @@ function get_input_data(num, col, is_seletion, seletion_name) {
     }
 }
 
+function People(i) {
+    return {
+        'name': get_name(i),
+        'gender': get_gender(i),
+        'position': get_position(i),
+        'votes': get_votes(i)
+    }
+}
+
 function test() {
-    get_name(1);
-    get_gender(1);
-    get_position(1)
-    get_votes(1);
+    get_gender_limits()
+    get_position_limit()
+    // alert(get_how_many_people_needed());
+    // s = People(1);
+    // alert(s.name)
+    // get_name(1);
+    // get_gender(1);
+    // get_position(1)
+    // get_votes(1);
 }
