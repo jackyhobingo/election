687134cf4a561f96fa6406b9dab6224cda2dd753
jackyhobingo <jackyhobingo@gmail.com>
2016-08-01 15:55:27 +0800

    Recode the project

--- js/compute.js
+++ js/compute.js
@@ -1,217 +1,299 @@
-var count = 4;
-var condition_count = 4;
-var candidate_count = 1;
+var num = 0;
 
-function sort_by_votes(a, b) {
-    return b.votes - a.votes;
-}
+var UP = 38;
+var DOWN = 40;
+var LEFT = 37;
+var RIGHT = 39;
 
-function is_gender_should_be_considered(limits) {
-    if (limits.gender == 'none' || is_limits_number_error(limits)) {
-        return false;
-    }
-    return true;
-}
+$(document).ready(function() {
+    $(function() {
+        $("div#candidate").on("keydown", function(evt) {
+            var tabIndex = $(":focus").attr('id');
+            var tabIndexInt = parseInt(tabIndex.slice(5));
+            switch (evt.which) {
+                // To Do Need to Refactor
+                case UP:
+                    if (tabIndexInt - 10 > 10) {
+                        $("#index" + String(tabIndexInt - 10)).focus();
+                    } else if (tabIndexInt - 10 > 0) {
+                        $("#index0" + String(tabIndexInt - 10)).focus();
+                    } else {
+                        $("#" + tabIndex).focus();
+                    }
+                    break;
+                case DOWN:
+                    if (tabIndexInt + 10 < num * 10) {
+                        $("#index" + String(tabIndexInt + 10)).focus();
+                    } else { 
+                        $("#" + tabIndex).focus();
+                    }
 
-function is_position_should_be_considered(limits) {
-    if (limits.position == 'none' || is_limits_number_error(limits)) {
-        return false;
-    }
-    return true;
-}
-
-
-function is_limits_number_error(limits) {
-    // test NaN and numerator > denominator
-    if (limits.denominator !== limits.denominator) {
-        return true;
-    } else if (limits.numerator !== limits.numerator) {
-        return true;
-    } else if (limits.numerator > limits.denominator) {
-        return true;
-    }
-    return false;
-}
-
-function show_ans(sorted_p_array) {
-    $('.selected').removeClass("selected");
-    for (var i=0 ; i < candidate_count; i ++) {
-        if( sorted_p_array[i].selected) {
-            $('tr.num'+sorted_p_array[i].num).addClass("selected");
-        } 
-    }
-
-}
-
-
-function compute_how_many_gender_prioritized_people(limits) {
-    if (!is_gender_should_be_considered(limits)) {
-        return 0;
-    } else {
-        return Math.ceil(candidate_count * limits.numerator / limits.denominator);
-    }
-}
-
-function compute_how_many_position_prioritized_people(limits) {
-    if (!is_position_should_be_considered(limits)) {
-        return 0;
-    } else {
-        return Math.ceil(candidate_count * limits.numerator / limits.denominator);
-    }
-}
-
-
-function get_gender_limits() {
-    return {
-        "gender": $('select#gender_limit').val(),
-        "numerator": parseInt($('input#gender_limit_son').val()),
-        "denominator": parseInt($('input#gender_limit_mom').val())
-    }
-}
-
-function get_position_limits() {
-    return {
-        "position": $('select#position_limit').val(),
-        "numerator": parseInt($('input#position_limit_son').val()),
-        "denominator": parseInt($('input#position_limit_mom').val())
-    }
-}
-
-function compute() {
-    var p = Array(candidate_count);
-    for (var i = 0; i < candidate_count; i++) {
-        p[i] = People(i + 1);
-    }
-    p.sort(sort_by_votes)
-    var gender_limit = get_gender_limits();
-    var gender_prioritized_people_amount = compute_how_many_gender_prioritized_people(gender_limit);
-    var position_limit = get_position_limits();
-    var position_prioritized_people_amount = compute_how_many_position_prioritized_people(position_limit);
-    var people_needed_amount = get_how_many_people_needed();
-
-    first_filter = get_filter("gender", gender_limit.gender, gender_prioritized_people_amount);
-    second_filter = get_filter("position", gender_limit.position, position_prioritized_people_amount);
-
-    p = selection(p, people_needed_amount, first_filter, second_filter);
-    show_ans(p)
-    // for ( var i = 0 ;  i < candidate_count ; i++ ){
-    //     if (p[i].selected){
-    //         alert(p[i].name);
-
-    //     }
-    // }
-
-
-}
-
-function get_filter (limit, limit_type, amount) {
-    return [limit, limit_type, amount]
-}
-
-function selection(sorted_p_array, needed_amount, first_filter, second_filter) {
-    var had_selected = 0;
-    for(var i = 0; i < candidate_count && first_filter[2] > 0; i++){
-        if (sorted_p_array[i][first_filter[0]] == first_filter[1]) {
-            sorted_p_array[i].selected = true;
-            had_selected ++;
-            first_filter[2] --;
-            if (needed_amount == had_selected) {
-                return sorted_p_array;
+                    break;
+                case LEFT:
+                    if (tabIndexInt % 10 == 1) { //bound
+                        $("#" + tabIndex).focus();
+                    } else {
+                        if (tabIndexInt > 10)
+                            $("#index" + String(tabIndexInt - 1)).focus();
+                        else
+                            $("#index0" + String(tabIndexInt - 1)).focus();
+                    }
+                    break;
+                case RIGHT:
+                    if (tabIndexInt % 10 == 4) { //bound
+                        $("#" + tabIndex).focus();
+                    } else {
+                        if (tabIndexInt > 10)
+                            $("#index" + String(tabIndexInt + 1)).focus();
+                        else
+                            $("#index0" + String(tabIndexInt + 1)).focus();
+                    }
+                    break;
             }
+        })
+    });
+    $('button#create-form').click(function() {
+        $('div#candidate').html("")
+        num = get_total_people();
+        for (var i = 0; i < num; i++) {
+            $('div#candidate').append('\
+                <div class="container">\
+                    <div class="row num' + i + '">\
+                        <div class="col-md-2 col-sm-2 col-xs-3">\
+                            <input class="form-control vote " id="index' + i + '1"  type="text" placeholder="票數">\
+                        </div>\
+                        <div class="col-md-2 col-sm-2  col-xs-3">\
+                            <input class="form-control name" id="index' + i + '2"  type="text" placeholder="姓名">\
+                        </div>\
+                        <div class="col-md-1 col-sm-1  col-xs-2">\
+                            <button class="btn gender btn-primary" id="index' + i + '3">男</button>\
+                        </div>\
+                        <div class="col-md-1 col-sm-1  col-xs-2">\
+                            <button class="btn position" id="index' + i + '4">一般</button>\
+                        </div>\
+                    </div>\
+                </div>');
+        };
+        form_button_event();
+
+    });
+
+    $('button#gender-limit').click(function() {
+
+        if ($(this).text().indexOf('無') > 0) {
+            $(this).html('性別限制：女性至少<input class="in-btn" type="text" id="gender-numerator">分之<input class="in-btn" id="gender-fraction" type="text">以上');
+            $(this).addClass("btn-info");
         }
-    }
-    for (var i=0; i < candidate_count && second_filter[2] > 0; i++) {
-        if (sorted_p_array[i][second_filter[0]] == second_filter[1]) {
-            if(!sorted_p_array[i].selected) {
-                sorted_p_array[i].selected = true;
-                had_selected ++;
-            }
-            second_filter[2] --;
-            if (needed_amount == had_selected) {
-                return sorted_p_array; 
-            }
+
+    });
+
+    $('button#gender-limit').dblclick(function() {
+
+        if ($(this).text().indexOf('女') > 0) {
+            $(this).html('性別限制：無');
+            $(this).removeClass("btn-info");
         }
-    }
-    for (var i=0; i< candidate_count; i++) {
-        if(!sorted_p_array[i].selected) {
-            sorted_p_array[i].selected = true;
-            had_selected ++;
-            if (needed_amount == had_selected) {
-                return sorted_p_array
-            }
+
+    })
+
+    $('button#position-limit').click(function() {
+
+        if ($(this).text().indexOf('無') > 0) {
+            $(this).html('職位限制：行政至少<input class="in-btn" type="text" id="position-numerator">分之<input class="in-btn" type="text" id="position-fraction">以上');
+            $(this).addClass("btn-info");
         }
+
+    });
+
+    $('button#position-limit').dblclick(function() {
+
+        if ($(this).text().indexOf('行政') > 0) {
+            $(this).html('職位限制：無');
+            $(this).removeClass("btn-info");
+        }
+
+    });
+
+})
+
+function get_total_people() {
+    return parseInt($('input#total-people').val());
+}
+
+function get_needed_people_amount() {
+    return parseInt($('input#need-people').val());
+}
+
+function PeopleArray(num) {
+
+    function sort_ascending_order_in_votes(a, b) {
+        return b.votes - a.votes;
     }
-    return sorted_p_array;
-}
 
-
-
-
-
-function add_condition() {
-
-}
-
-function add_candidate(num) {
-    $("tr.num" + num).after('\
-        <tr class="num' + (num + 1) + '">\
-            <td><input class="num' + (num + 1) + '" id="num' + (num + 1) + '_col1" type="text"></td>\
-            <td><input class="num' + (num + 1) + ' gender' + (num + 1) + '" id="num' + (num + 1) + '_col2" type=radio name="gender' + (num + 1) + '" value="man"></td>\
-            <td class="gender' + (num + 1) + '">男</td>\
-            <td><input class="num' + (num + 1) + ' gender' + (num + 1) + '" id="num' + (num + 1) + '_col2" type=radio value="woman" name="gender' + (num + 1) + '"></td>\
-            <td class="gender' + (num + 1) + '">女</td>\
-            <td><input class="num' + (num + 1) + ' position' + (num + 1) + '" id="num' + (num + 1) + '_col3" type=radio name="position' + (num + 1) + '" value="normal"></td>\
-            <td class="position' + (num + 1) + '">一般</td>\
-            <td><input class="num' + (num + 1) + ' position' + (num + 1) + '" id="num' + (num + 1) + '_col3" type=radio value="admin" name="position' + (num + 1) + '"></td>\
-            <td class="position' + (num + 1) + '">行政</td>\
-            <td><input class="num' + (num + 1) + '" id="num' + (num + 1) + '_col4" type="text"></td>\
-        </tr>')
-    candidate_count += 1;
-}
-
-function add_election_condition() {
-    // To do
-}
-
-function get_how_many_people_needed() {
-    return parseInt($('input#how_many').val());
-}
-
-function get_votes(i) {
-    return parseInt(get_input_data(i, 4, false, 0));
-}
-
-function get_gender(i) {
-    return get_input_data(i, 2, true, "gender" + i);
-}
-
-function get_name(i) {
-    return get_input_data(i, 1, false, 0);
-}
-
-function get_position(i) {
-    return get_input_data(i, 3, true, "position" + i);
-}
-
-function get_input_data(num, col, is_seletion, seletion_name) {
-    if (!is_seletion) {
-        return $('input#num' + num + '_col' + col).val();
-    } else {
-        return $("input.num" + num + ":radio:checked[name='" + seletion_name + "']").val();
+    var p = Array(num);
+    for (var i = 0; i < num; i++) {
+        p[i] = People(i);
     }
+    p.sort(sort_ascending_order_in_votes);
+    return p;
 }
 
 function People(i) {
+
+    function get_votes(num) {
+        return parseInt($('div.num' + num).children('div').children("input.vote").val());
+    }
+
+    function get_name(num) {
+        return $('div.num' + num).children('div').children("input.name").val();
+    }
+
+    function get_gender(num) {
+        return $('div.num' + num).children('div').children("button.gender").text();
+    }
+
+    function get_position(num) {
+        return $('div.num' + num).children('div').children("button.position").text();
+    }
+
     return {
+        'votes': get_votes(i),
         'name': get_name(i),
         'gender': get_gender(i),
         'position': get_position(i),
-        'votes': get_votes(i),
         'selected': false,
         'num': i
     }
+
 }
 
-function test() {
 
+
+function get_total_woman() {
+    var total = 0
+    for (var i = 0; i < num; i++) {
+
+        if ($('div.num' + i).children('div').children("button.gender").text() === '女') {
+            total++;
+        }
+
+    }
+    return total;
+}
+
+
+
+
+
+function get_gender_limit() {
+    var limit = get_limit('gender');
+    var w = get_total_woman();
+    var total = get_total_people();
+    var need_amount = get_needed_people_amount();
+    if (w / total < limit)
+        return Math.ceil(need_amount * w / total);
+    else
+        return Math.ceil(need_amount * limit);
+}
+
+function get_position_limit() {
+    var limit = get_limit('position');
+    var need_amount = get_needed_people_amount();
+    return Math.ceil(need_amount * limit);
+}
+
+function get_limit(type) {
+
+    var n = parseInt($('button#' + type + '-limit').children('input#' + type + '-numerator').val());
+    var f = parseInt($('button#' + type + '-limit').children('input#' + type + '-fraction').val());
+    ans = f / n;
+    if (ans !== ans || ans >= 1) {
+        ans = 0;
+    }
+
+    return ans;
+}
+
+
+
+
+
+function form_button_event() {
+    $('button.gender').click(function() {
+        if ($(this).text() === '男') {
+            $(this).html('女');
+            $(this).addClass("btn-danger");
+            $(this).removeClass("btn-primary");
+        } else if ($(this).text() === '女') {
+            $(this).html('男');
+            $(this).addClass("btn-primary");
+            $(this).removeClass("btn-danger");
+
+        }
+    });
+
+
+    $('button.position').click(function() {
+        if ($(this).text() === '一般') {
+            $(this).html('行政');
+            $(this).addClass("btn-warning");
+        } else if ($(this).text() === '行政') {
+            $(this).html('一般');
+            $(this).removeClass("btn-warning");
+        }
+    });
+
+    $('button#compute').click(function() {
+        var p = PeopleArray(num);
+        var gender_limit = get_gender_limit();
+        var gender_chosen = 0;
+        var position_limit = get_position_limit();
+        var position_chosen = 0;
+        var need_amount = get_needed_people_amount();
+        var total_chosen = 0;
+
+        // gender priority
+        for (var i = 0; i < num && need_amount > total_chosen && gender_limit > gender_chosen; i++) {
+            if (!p[i].selected && p[i].gender === '女') {
+                p[i].selected = true;
+                total_chosen++;
+                gender_chosen++;
+                if (p[i].position === '行政') {
+                    position_chosen++;
+                }
+            }
+        }
+        // position priority
+        for (var i = 0; i < num && need_amount > total_chosen && position_limit > position_chosen; i++) {
+            if (!p[i].selected && p[i].position === '行政') {
+                p[i].selected = true;
+                total_chosen++;
+                position_chosen++;
+            }
+        }
+        // left 
+        for (var i = 0; i < num && need_amount > total_chosen; i++) {
+            if (!p[i].selected) {
+                p[i].selected = true;
+                total_chosen++;
+            }
+        }
+
+        // clear 
+        $('div#result').html('');
+
+        // show the result
+        for (var i = 0; i < num; i++) {
+            if (p[i].selected) {
+                $('div#result').append('\
+                        <div class="container">\
+                            <div class="row">\
+                                <div class="col-md-2 col-sm-2  col-xs-3">\
+                                    ' + p[i].name + '\
+                                </div>\
+                            </div>\
+                        </div>')
+            }
+        }
+    });
 }
